2d575b41f2a421f23546d83438f905822d6b5cfb
Niti Assavaplakorn <nitikub@gmail.com>
2020-11-17 15:04:15 +0700

    add debug

--- server.py
+++ server.py
@@ -14,10 +14,10 @@ from kaldi import KaldiSink, kaldi_server_queue
 ROOT = Path(__file__).parent
 
 root = logging.getLogger()
-root.setLevel(logging.INFO)
+root.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('[%(asctime)s] %(name)s <%(levelname)s> %(message)s')
 handler.setFormatter(formatter)
 root.addHandler(handler)
